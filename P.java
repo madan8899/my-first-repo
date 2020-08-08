d5273b3fb2776f3a23f4bd28e7e83b72a1135660
Madan <madangupta8899@gmail.com>
2020-08-08 19:50:10 +0530

    added gitignore file

---
+++ P.java
@@ -0,0 +1,36 @@
+// class P	{
+	// public static void main(String args[])
+	// {
+		// System.out.println("main of P");
+	// }
+// }
+
+// class C extends P
+// {
+// }
+
+
+
+
+// class Test
+// {
+	// final static strictfp synchronized public void main(String... s)
+	// {
+		// System.out.println("it's valid main");
+	// }
+// }
+
+
+
+
+// class P	{
+	// public static void main(String args[])
+	// {
+		// System.out.println("main with String args");
+	// }
+	
+	// public static void main(int args[])
+	// {
+		// System.out.println("main with int args");
+	// }
+// }
